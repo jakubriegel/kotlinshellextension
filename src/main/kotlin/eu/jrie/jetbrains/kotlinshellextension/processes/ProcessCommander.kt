a1de2b1111f10aa4103272f56a341276eff1d558
jakubriegel <6ni5meFI!>
2019-07-05 11:08:31 +0200

    ProcessCommander code optimisations

--- src/main/kotlin/eu/jrie/jetbrains/kotlinshellextension/processes/ProcessCommander.kt
+++ src/main/kotlin/eu/jrie/jetbrains/kotlinshellextension/processes/ProcessCommander.kt
@@ -1,41 +1,33 @@
 package eu.jrie.jetbrains.kotlinshellextension.processes
 
-import eu.jrie.jetbrains.kotlinshellextension.processes.process.PCB
 import eu.jrie.jetbrains.kotlinshellextension.processes.process.Process
 import eu.jrie.jetbrains.kotlinshellextension.processes.process.ProcessBuilder
 import eu.jrie.jetbrains.kotlinshellextension.processes.process.ProcessConfiguration
 import org.apache.log4j.BasicConfigurator
-import java.lang.Exception
 
 class ProcessCommander {
 
     private val processes = mutableSetOf<Process>()
 
-    fun systemProcess(config: ProcessConfiguration.() -> Unit): Int {
-        val process = with(ProcessConfiguration().apply(config)) {
+    fun systemProcess(config: ProcessConfiguration.() -> Unit) =
+        createSystemProcess(config).let {
+            processes.add(it)
+            it.virtualPID
+        }
+
+    private fun createSystemProcess(config: ProcessConfiguration.() -> Unit) =
+        with(ProcessConfiguration().apply(config)) {
             ProcessBuilder.createSystemProcess(this, virtualPID())
         }
 
-        processes.add(process)
-        return process.virtualPID
-    }
+    fun startProcess(vPID: Int) = getProcessByVirtualPID(vPID).start()
 
-    fun startProcess(vPID: Int): PCB {
-        val process = getProcessByVirtualPID(vPID)
-        return process.start()
-    }
+    fun awaitProcess(vPID: Int, timeout: Long = 0) = getProcessByVirtualPID(vPID).await(timeout)
 
-    fun awaitProcess(vPID: Int, timeout: Long = 0) {
-        getProcessByVirtualPID(vPID).await(timeout)
-    }
-
-    fun killProcess(vPID: Int) {
-        val process = getProcessByVirtualPID(vPID)
-        process.kill()
-    }
+    fun killProcess(vPID: Int) = getProcessByVirtualPID(vPID).kill()
 
     private fun getProcessByVirtualPID(vPID: Int) =
-        processes.find { it.virtualPID == vPID } ?: throw Exception("no processes with given virtual PID; $vPID")
+        processes.find { it.virtualPID == vPID } ?: throw Exception("no processes with given virtual PID: $vPID")
 
     companion object {
         init {
