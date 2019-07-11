a815a6f977a6b3559602150e1debf765eff3dc83
jakubriegel <6ni5meFI!>
2019-07-11 16:39:48 +0200

    TestDatFactory cleanup

--- src/test/kotlin/eu/jrie/jetbrains/kotlinshellextension/testutils/TestDataFactory.kt
+++ src/test/kotlin/eu/jrie/jetbrains/kotlinshellextension/testutils/TestDataFactory.kt
@@ -1,12 +1,5 @@
 package eu.jrie.jetbrains.kotlinshellextension.testutils
 
-import eu.jrie.jetbrains.kotlinshellextension.processes.process.ProcessConfiguration
-import eu.jrie.jetbrains.kotlinshellextension.processes.process.stream.ProcessInputStream
-import eu.jrie.jetbrains.kotlinshellextension.processes.process.stream.ProcessOutputStream
-import io.mockk.mockk
-import io.mockk.spyk
-import kotlinx.coroutines.CoroutineScope
-
 object TestDataFactory {
     // constants
     const val VIRTUAL_PID = 1
@@ -20,18 +13,4 @@ object TestDataFactory {
     val ENVIRONMENT = mapOf(ENV_VAR_1, ENV_VAR_2, ENV_VAR_3)
 
     const val SYSTEM_PID: Long = 1001
-
-    // mocks and spies
-    val SCOPE_MOCK = mockk<CoroutineScope>()
-    fun processInputStreamSpy() = spyk(ProcessInputStream(SCOPE_MOCK))
-    fun processOutputStreamSpy() = spyk(ProcessOutputStream(SCOPE_MOCK))
-
-    // other data
-    fun processConfigFunction(): ProcessConfiguration.() -> Unit {
-        return {
-            cmd {
-                PROCESS_COMMAND withArgs PROCESS_ARGS
-            }
-        }
-    }
 }
