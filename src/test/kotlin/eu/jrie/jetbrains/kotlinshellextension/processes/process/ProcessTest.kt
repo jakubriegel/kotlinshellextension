a95e5e61f56891105f46a4c5cca4d7dfa013bed3
jakubriegel <6ni5meFI!>
2019-07-11 16:22:04 +0200

    Process api converted to new architecture

--- src/test/kotlin/eu/jrie/jetbrains/kotlinshellextension/processes/process/ProcessTest.kt
+++ src/test/kotlin/eu/jrie/jetbrains/kotlinshellextension/processes/process/ProcessTest.kt
@@ -1,17 +1,10 @@
 package eu.jrie.jetbrains.kotlinshellextension.processes.process
 
-import eu.jrie.jetbrains.kotlinshellextension.processes.process.stream.ProcessInputStream
-import eu.jrie.jetbrains.kotlinshellextension.processes.process.stream.ProcessOutputStream
+import eu.jrie.jetbrains.kotlinshellextension.processes.process.stream.ProcessStream
 import eu.jrie.jetbrains.kotlinshellextension.testutils.TestDataFactory.ENVIRONMENT
-import eu.jrie.jetbrains.kotlinshellextension.testutils.TestDataFactory.ENV_VAR_1
-import eu.jrie.jetbrains.kotlinshellextension.testutils.TestDataFactory.ENV_VAR_2
-import eu.jrie.jetbrains.kotlinshellextension.testutils.TestDataFactory.ENV_VAR_3
-import eu.jrie.jetbrains.kotlinshellextension.testutils.TestDataFactory.processInputStreamSpy
-import eu.jrie.jetbrains.kotlinshellextension.testutils.TestDataFactory.processOutputStreamSpy
-import io.mockk.Runs
 import io.mockk.every
 import io.mockk.just
-import io.mockk.mockk
+import io.mockk.runs
 import io.mockk.spyk
 import io.mockk.verify
 import kotlinx.coroutines.Job
@@ -22,141 +15,22 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.EnumSource
+import java.io.File
 
 class ProcessTest {
 
     private val process = spyk<SampleProcess>()
 
-    private companion object {
+    companion object {
         const val COMMAND = "cmd"
         const val VIRTUAL_PID = 1
-    }
 
-    @Test
-    fun `should redirect input`() {
-        // when
-        process.followIn()
-
-        // then
-        verify {
-            process.followIn(ofType(ProcessInputStream::class))
+        val stdMock = spyk<ProcessStream> {
+            every { close() } just runs
         }
-    }
-
-    @Test
-    fun `should redirect input to given stream`() {
-        // given
-        val inputSpy = processInputStreamSpy()
-
-        // when
-        process.followIn(inputSpy)
-
-        // then
-        assertEquals(process.input, inputSpy)
-        assertEquals(process.vPID, inputSpy.vPID)
-    }
-
-    @Test
-    fun `should redirect merged output to stdout`() {
-        // when
-        process.followMergedOut()
-
-        // then
-        verify (exactly = 1) {
-            process.followMergedOut(ofType(ProcessOutputStream::class))
+        val errMock = spyk<ProcessStream> {
+            every { close() } just runs
         }
-    }
-
-    @Test
-    fun `should redirect merged output to given stream`() {
-        // given
-        val outSpy = processOutputStreamSpy()
-
-        // when
-        process.followMergedOut(outSpy)
-
-        // then
-        assertEquals(process.stdout, outSpy)
-        assertEquals(process.vPID, outSpy.vPID)
-    }
-
-    @Test
-    fun `should redirect outputs to correct destinations`() {
-        // when
-        process.followOut()
-
-        // then
-        verify (exactly = 1) { process.followOut() }
-        verify (exactly = 1) { process.followStdOut() }
-        verify (exactly = 1) { process.followStdErr() }
-        verify (exactly = 1) { process.followStdOut(ofType(ProcessOutputStream::class)) }
-        verify (exactly = 1) { process.followStdErr(ofType(ProcessOutputStream::class)) }
-    }
-
-    @Test
-    fun `should redirect outputs to given destinations`() {
-        // given
-        val stdSpy = processOutputStreamSpy()
-        val errSpy = processOutputStreamSpy()
-
-        // when
-        process.followOut(stdSpy, errSpy)
-
-        // then
-        verify (exactly = 1) { process.followStdOut(stdSpy) }
-        verify (exactly = 1) { process.followStdErr(errSpy) }
-        assertEquals(process.stdout, stdSpy)
-        assertEquals(process.stderr, errSpy)
-        assertEquals(process.vPID, stdSpy.vPID)
-        assertEquals(process.vPID, errSpy.vPID)
-    }
-
-    @Test
-    fun  `should add new variable to the environment`() {
-        // when
-        process.addEnv(ENV_VAR_1)
-
-        // then
-        assertEquals(process.environment().size, 1)
-        assertEquals(process.environment()[ENV_VAR_1.first], ENV_VAR_1.second)
-    }
-
-    @Test
-    fun  `should add new variables to the environment`() {
-        // when
-        process.addEnv(ENVIRONMENT)
-
-        // then
-        assertEquals(process.environment().size, 3)
-        assertEquals(process.environment()[ENV_VAR_1.first], ENV_VAR_1.second)
-        assertEquals(process.environment()[ENV_VAR_2.first], ENV_VAR_2.second)
-        assertEquals(process.environment()[ENV_VAR_3.first], ENV_VAR_3.second)
-    }
-
-    @Test
-    fun  `should replace the environment with given variable`() {
-        // when
-        process.addEnv(ENV_VAR_1)
-        process.addEnv(ENV_VAR_2)
-        process.setEnv(ENV_VAR_3)
-
-        // then
-        assertEquals(process.environment().size, 1)
-        assertEquals(process.environment()[ENV_VAR_3.first], ENV_VAR_3.second)
-    }
-
-    @Test
-    fun  `should replace the environment with given variables`() {
-        // when
-        process.addEnv("var1" to "val2")
-        process.addEnv("var2" to "val2")
-        process.setEnv(ENVIRONMENT)
-
-        // then
-        assertEquals(process.environment().size, 3)
-        assertEquals(process.environment()[ENV_VAR_1.first], ENV_VAR_1.second)
-        assertEquals(process.environment()[ENV_VAR_2.first], ENV_VAR_2.second)
-        assertEquals(process.environment()[ENV_VAR_3.first], ENV_VAR_3.second)
     }
 
     @Test
@@ -220,7 +94,7 @@ class ProcessTest {
     }
 
     @Test
-    fun `should await process`(): Unit = runBlocking {
+    fun `should await process`() = runBlocking {
         val timeout: Long = 500
         every { process.expect(timeout) } returns launch { }
 
@@ -233,16 +107,6 @@ class ProcessTest {
 
     @Test
     fun `should close stdout and stderr`() {
-        // given
-        val stdMock = spyk(ProcessOutputStream(mockk())) {
-            every { close() } just Runs
-        }
-        val errMock = spyk(ProcessOutputStream(mockk())) {
-            every { close() } just Runs
-        }
-
-        process.followOut(stdMock, errMock)
-
         // when
         process.closeOut()
 
@@ -251,24 +115,22 @@ class ProcessTest {
         verify (exactly = 1) { errMock.close() }
     }
 
-    private open class SampleProcess : Process(VIRTUAL_PID, COMMAND, scope = mockk()) {
-
+    private open class SampleProcess : Process(
+        VIRTUAL_PID,
+        spyk(),
+        stdMock,
+        errMock,
+        ENVIRONMENT,
+        File(""),
+        spyk()
+    ) {
         override val pcb: PCB = spyk()
+        override val statusCmd = ""
+        override val statusOther = ""
 
-        override fun redirectIn(source: ProcessInputStream) {}
-
-        override fun redirectMergedOut(destination: ProcessOutputStream) {}
-
-        override fun redirectStdOut(destination: ProcessOutputStream) {}
-
-        override fun redirectStdErr(destination: ProcessOutputStream) {}
-
-        override fun execute(): PCB = mockk()
-
+        override fun execute(): PCB = spyk()
         override fun isAlive(): Boolean = false
-
-        override fun expect(timeout: Long): Job = mockk()
-
+        override fun expect(timeout: Long): Job = spyk()
         override fun destroy() {}
     }
 
