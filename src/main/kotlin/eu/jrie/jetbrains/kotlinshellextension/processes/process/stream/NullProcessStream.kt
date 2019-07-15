bee9e3ee88c5e86b8cca3b1e6d229ed8883ec8d7
jakubriegel <6ni5meFI!>
2019-07-15 11:14:31 +0200

    NullProcessStream and ProcessListener Tests

--- src/main/kotlin/eu/jrie/jetbrains/kotlinshellextension/processes/process/stream/NullProcessStream.kt
+++ src/main/kotlin/eu/jrie/jetbrains/kotlinshellextension/processes/process/stream/NullProcessStream.kt
@@ -2,8 +2,15 @@ package eu.jrie.jetbrains.kotlinshellextension.processes.process.stream
 
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.channels.Channel
+import org.jetbrains.annotations.TestOnly
 
-class NullProcessStream : ProcessStream() {
+class NullProcessStream @TestOnly internal constructor(
+    channel: Channel<Byte>
+) : ProcessStream(channel) {
+
+    constructor() : this(Channel(CHANNEL_BUFFER_SIZE))
+
     override fun write(line: String) {}
 
     override fun writeAsLine(data: String) {}
