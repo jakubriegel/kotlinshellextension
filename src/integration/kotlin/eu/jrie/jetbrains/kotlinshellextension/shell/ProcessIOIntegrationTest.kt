6defa1711cf30fbd9e415894016d8e351d882dab
jakubriegel <6ni5meFI!>
2019-07-22 09:18:56 +0200

    ProcessIOIntegrationTest completed missing cases

--- src/integration/kotlin/eu/jrie/jetbrains/kotlinshellextension/shell/ProcessIOIntegrationTest.kt
+++ src/integration/kotlin/eu/jrie/jetbrains/kotlinshellextension/shell/ProcessIOIntegrationTest.kt
@@ -1,7 +1,10 @@
 package eu.jrie.jetbrains.kotlinshellextension.shell
 
 import eu.jrie.jetbrains.kotlinshellextension.shell
+import eu.jrie.jetbrains.kotlinshellextension.stdout
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.io.core.BytePacketBuilder
+import kotlinx.io.core.ByteReadPacket
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeAll
@@ -74,37 +77,45 @@ class ProcessIOIntegrationTest : ProcessBaseIntegrationTest() {
 
     @Test
     fun `should print stdout to console and stderr to given buffer when only stderr buffer given`() {
-        TODO()
-//        // when
-//        shell {
-//            launchSystemProcess {
-//                cmd = "./${script.name}"
-//                dir(directory)
-//            }
-//
-//            commander.awaitAll()
-//        }
-//
-//        // then
-//        assertEquals(printScriptStdOut(n), outFile.withoutLogs())
-//        assertEquals(printScriptStdErr(n), readResult())
+        // when
+        shell {
+            val script = systemProcess {
+                cmd = "./${script.name}"
+                dir(directory)
+            }
+
+            (script forkErr { it pipe storeResult }) pipe stdout
+
+            commander.awaitAll()
+        }
+
+        // then
+        assertEquals(printScriptStdOut(n), outFile.withoutLogs())
+        assertEquals(printScriptStdErr(n), readResult())
     }
 
     @Test
     fun `should print stdout and stderr to given buffer when both buffers given`() {
-        TODO()
-//        // when
-//        shell {
-//            launchSystemProcess {
-//                cmd = "./${script.name}"
-//                dir(directory)
-//            }
-//
-//            commander.awaitAll()
-//        }
-//
-//        // then
-//        TODO("assert")
+        // given
+        val stdBuilder = BytePacketBuilder()
+        val storeStd = { it: ByteReadPacket -> stdBuilder.writePacket(it) }
+
+        // when
+        shell {
+            val script = systemProcess {
+                cmd = "./${script.name}"
+                dir(directory)
+            }
+
+            (script forkErr { it pipe storeResult }) pipe storeStd
+
+            commander.awaitAll()
+        }
+
+        // then
+        assertEquals("", outFile.withoutLogs())
+        assertEquals(printScriptStdOut(n), stdBuilder.build().readText())
+        assertEquals(printScriptStdErr(n), readResult())
     }
 
     private fun File.withoutLogs()=  StringBuilder().let { b ->
